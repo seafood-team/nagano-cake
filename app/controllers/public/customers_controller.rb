9cd007458b949bf375515bf6bf04c4438f92a529
karei2640 <121594678+karei2640@users.noreply.github.com>
2023-03-14 16:54:37 +0900

    Update customers_controller.rb

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -2,7 +2,7 @@ class Public::CustomersController < ApplicationController
   before_action :authenticate_customer!
   
   def show
-    @customer = Customer.find(params[:id])
+    @customer = current_customer
   end
 
   def unsubscribe
