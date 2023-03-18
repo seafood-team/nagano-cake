a95c67d069e32a12177fb0f25f11d480b9978b4f
yuiboo1025 <122716372+yuiboo1025@users.noreply.github.com>
2023-03-18 05:01:47 +0000

    [Add]カート機能実装追加

--- app/controllers/public/sessions_controller.rb
+++ app/controllers/public/sessions_controller.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class Public::SessionsController < Devise::SessionsController
-  before_action :reject_inactive_customer, only: [:create]
+  before_action :reject_withdraw_customer, only: [:create]
   # before_action :configure_sign_in_params, only: [:create]
 
   # GET /resource/sign_in
@@ -25,12 +25,13 @@ class Public::SessionsController < Devise::SessionsController
   # def configure_sign_in_params
   #   devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
   # end
-  def reject_inactive_user
-    @customer = Customer.find_by(name: params[:name])
-    if @customer
-      if @customer.valid_password?(params[:user][:password]) && !@customer.is_valid
-        redirect_to new_customer_session_path
-      end
-    end
+  def reject_withdraw_customer
+  @customer = Customer.find_by(email: params[:customer][:email].downcase)
+  if @customer
+   if (@customer.valid_password?(params[:customer][:password]) && (@customer.active_for_authentication? == false))
+    flash[:notice] = "退会済みのためログインできません。"
+    redirect_to new_customer_session_path
+   end
   end
+ end
 end
