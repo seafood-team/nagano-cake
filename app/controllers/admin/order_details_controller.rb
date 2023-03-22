d6814eb11034886d9aae4a424a62de2acd8fdf56
yuiboo1025 <122716372+yuiboo1025@users.noreply.github.com>
2023-03-22 03:49:59 +0000

    [Add]注文一覧表示追加

--- app/controllers/admin/order_details_controller.rb
+++ app/controllers/admin/order_details_controller.rb
@@ -2,6 +2,13 @@ class Admin::OrderDetailsController < ApplicationController
   
 
   def update
+    @order_detail = OrderDetail.find(params[:id])
+    @order_detail.update(order_detail_params)
+    redirect_to request.referer
+  end
+  
+  def order_detail_params
+    params.require(:order_detail).permit(:making_status,:amount,:price)
   end
   
 end
