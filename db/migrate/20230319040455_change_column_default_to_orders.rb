ca33c26d170c6b320e405f0de616ef0ee8932fd0
geromon <123849679+geromon89@users.noreply.github.com>
2023-03-22 08:02:55 +0000

    [Add]注文詳細の更新

--- db/migrate/20230319040455_change_column_default_to_orders.rb
+++ db/migrate/20230319040455_change_column_default_to_orders.rb
@@ -1,6 +1,5 @@
 class ChangeColumnDefaultToOrders < ActiveRecord::Migration[6.1]
   def change
-    change_column_default :orders, :payment_method, from: nil, to: "0"
     change_column_default :orders, :order_status, from: nil, to: "0"
   end
 end
