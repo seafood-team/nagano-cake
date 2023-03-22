0803f00d9a3d8342039c0b31c530908958fc5469
EC2 Default User <ec2-user@ip-172-31-13-18.ap-northeast-1.compute.internal>
2023-03-22 07:08:35 +0000

    [edit]商品削除機能追加

--- app/controllers/admin/genres_controller.rb
+++ app/controllers/admin/genres_controller.rb
@@ -29,6 +29,11 @@ class Admin::GenresController < ApplicationController
     end
   end
   
+  def destroy
+    @genre = Genre.find(params[:id])
+    @genre.destroy  
+    redirect_to admin_genres_path
+  end
  
   private
 
