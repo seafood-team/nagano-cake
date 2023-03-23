2dfe006201925a7538e131c0e1474dab77c56ba8
yuiboo1025 <122716372+yuiboo1025@users.noreply.github.com>
2023-03-23 11:50:16 +0000

    [Finish]注文一覧ステータス変更完了

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -14,7 +14,7 @@ class Admin::OrdersController < ApplicationController
     @order_details = OrderDetail.where(order_id: params[:id])
     if @order.update(order_params)
       @order_details.update_all(making_status: 1) if @order.order_status == "payment_confirmation"
-    ## ①注文ステータスが「入金確認」とき、製作ステータスを全て「製作待ち」に更新する
+
     end
     redirect_to admin_order_path(@order)
   end
