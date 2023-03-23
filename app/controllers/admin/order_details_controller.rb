2c60ba9e017d2cad92fccb9983d8eb09a8c71cec
yuiboo1025 <122716372+yuiboo1025@users.noreply.github.com>
2023-03-23 09:05:53 +0000

    [Add]enum連動化追加

--- app/controllers/admin/order_details_controller.rb
+++ app/controllers/admin/order_details_controller.rb
@@ -1,14 +1,27 @@
 class Admin::OrderDetailsController < ApplicationController
-  
+
 
   def update
+    @order = Order.find(params[:order_id])
     @order_detail = OrderDetail.find(params[:id])
-    @order_detail.update(order_detail_params)
-    redirect_to request.referer
+    @order_details = @order.order_details.all
+
+    is_updated = true
+    if @order_detail.update(order_detail_params)
+      @order.update(order_status: 2) if @order_detail.making_status == "making" 
+      @order_details.each do |order_detail| 
+        if order_detail.making_status != "finish_making" 
+          is_updated = false 
+        end
+      end
+      @order.update(order_status: 3) if is_updated
+      
+    end
+    redirect_to admin_order_path(@order)
   end
-  
+
   def order_detail_params
-    params.require(:order_detail).permit(:making_status,:amount,:price)
+    params.require(:order_detail).permit(:order_id, :making_status,:amount,:price)
   end
-  
+
 end
--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -11,10 +11,14 @@ class Admin::OrdersController < ApplicationController
 
   def update
     @order = Order.find(params[:id])
-    @order.update(order_params)
-    redirect_to request.referer
+    @order_details = OrderDetail.where(order_id: params[:id])
+    if @order.update(order_params)
+      @order_details.update_all(making_status: 1) if @order.order_status == "payment_confirmation"
+    ## ①注文ステータスが「入金確認」とき、製作ステータスを全て「製作待ち」に更新する
+    end
+    redirect_to admin_order_path(@order)
   end
-  
+
   def customer
     @customer_order = Customer.find(params[:customer_id])
   end
@@ -22,7 +26,7 @@ class Admin::OrdersController < ApplicationController
   def order_params
     params.require(:order).permit(:customer_id, :post_code, :address, :customer_name, :shipping_cost, :payment_method, :order_status)
   end
-  
+
   def order_detail_params
     params.require(:order_detail).permit(:making_status,:amount,:price)
   end
