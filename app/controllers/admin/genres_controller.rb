9a24e273527c832ef8ba6f0af0beac7f637dfa51
EC2 Default User <ec2-user@ip-172-31-13-18.ap-northeast-1.compute.internal>
2023-03-24 05:01:13 +0000

    ページネーション再設定、ジャンル削除機能を削除

--- app/controllers/admin/genres_controller.rb
+++ app/controllers/admin/genres_controller.rb
@@ -29,11 +29,7 @@ class Admin::GenresController < ApplicationController
     end
   end
   
-  def destroy
-    @genre = Genre.find(params[:id])
-    @genre.destroy  
-    redirect_to admin_genres_path
-  end
+  
  
   private
 
